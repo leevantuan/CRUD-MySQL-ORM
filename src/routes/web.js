118e591f421e3e0105881b906b2c2c4d9d2a876c
leevantuan <leevantuan2002@gmail.com>
2023-06-08 11:38:48 +0700

    Authorization

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const db = require('../models/index');
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const { getHomePage, getDemoEjs, getCreateUsers, getUpdateUsers, postUpdateUsers, getDeleteUsers, postDeleteUsers } = require('../controllers/homeController')
 const { postCreateUser } = require('../services/CRUDservices')
+const { LoginWithUsers, LoginWithAdmin, AuthTokenUsers, LogoutWithUsers, RefreshWithUsers, AuthTokenAdmin } = require('../services/Login')
 
 
 router.get('/', getHomePage)
@@ -13,21 +15,29 @@ router.get('/users', getDemoEjs)
 
 // router.get('/update/:UserId', getUpdateUsers)
 
-// router.get('/delete/:UserId', getDeleteUsers)
+// router.get('/delete/:UserId', getDeleteUsers)s
 
 
 router.post('/create-users',
     body('Phone').isLength({ min: 10 }).isInt(),
     body('PassWord').isLength({ min: 8 }),
+    body('Name').isLength({ max: 20 }),
     async (req, res) => {
         const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
         const data = req.body;
-        await postCreateUser(data);
+        if (!errors.isEmpty()) {
+            return res.status(200).json({ message: "Error phone number! Check your phone number, Please!" });
+        }
+        const CheckPhone = await db.User.findOne({ where: { Phone: data.Phone } })
+        if (CheckPhone) {
+            return res.status(200).json({ message: "Error phone number! Check your phone number, Please!" });
+        }
+        else {
+            await postCreateUser(data);
+        }
+        // console.log(data)
         // console.log(Name, Phone, PassWord, RoleID)
-        res.send("Create success!")
+        return res.status(200).json({ message: "success" });
     }
 )
 
@@ -35,5 +45,16 @@ router.put('/create-users', postUpdateUsers)
 
 router.delete('/create-users', postDeleteUsers)
 
+router.post('/login-users', LoginWithUsers)
+
+router.post('/check-user', AuthTokenUsers)
+
+// router.post('/refresh-users', RefreshWithUsers)
+
+// router.post('/logout-users', LogoutWithUsers)
+
+router.post('/login-admin', LoginWithAdmin)
+
+router.post('/check-admin', AuthTokenAdmin)
 
 module.exports = router;
---
+++ src/services/Login.js
@@ -0,0 +1,153 @@
+const db = require('../models/index');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config()
+
+// let RefreshTokens = [];
+
+const LoginWithUsers = async (req, res) => {
+    let data = req.body;
+
+    let user = await db.User.findOne({ where: { Phone: data.Phone } })
+
+    if (user) {
+        let check = await bcrypt.compareSync(data.PassWord, user.PassWord);
+        if (check) {
+
+            let token = jwt.sign({ data: { Name: user.Name, Phone: user.Phone, PassWord: "Not Show", RoleID: "Not Show" } }, process.env.JWT_SECRET, { expiresIn: '3 days' });
+
+            // let refreshToken = jwt.sign({ data: user }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7 days' });
+            // RefreshTokens.push(refreshToken);
+
+            return res.status(200).json({
+                message: "success",
+                token,
+                // refreshToken,
+            });
+        }
+        else {
+            return res.status(200).json({
+                message: "Error password! Check your password, Please!"
+            });
+        }
+    }
+
+    return res.status(200).json({ message: "Error phone number! Check your phone number, Please!" });
+}
+
+const AuthTokenUsers = (req, res, next) => {
+    const AuthHeader = req.headers['authorization'];
+    // const AuthHeader = req.body.body.Authorization;
+    // console.log(AuthHeader)
+    const token = AuthHeader.split(' ')[1];
+    if (!token) {
+        // res.status(401);
+        res.Status(200).json({ message: "Login field!" });
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+
+        if (err) {
+            res.status(403)
+        }
+        else {
+            // next();
+            res.status(200).json({ data })
+        }
+    })
+}
+
+
+const LogoutWithUsers = async (req, res) => {
+    const refreshToken = req.body.token;
+    // console.log(refreshToken)
+    RefreshTokens = await RefreshTokens.filter((refToken) => refToken !== refreshToken)
+    res.status(200).json(RefreshTokens)
+}
+
+const RefreshWithUsers = (req, res) => {
+    const refreshToken = req.body.token;
+    // res.send(RefreshTokens)
+    if (!refreshToken) {
+        res.status(401)
+    }
+    else {
+        const checkRefreshToken = RefreshTokens.includes(refreshToken)
+        if (checkRefreshToken === false) {
+            res.status(403)
+        }
+        else {
+            jwt.verify(
+                refreshToken, process.env.JWT_REFRESH_SECRET, (err, data) => {
+                    // console.log(data)
+                    if (err) {
+                        res.status(403)
+                    }
+                    else {
+                        const accessToken = jwt.sign({ data: { id: data.id, Name: data.Name, Phone: data.Phone } }, process.env.JWT_SECRET, { expiresIn: '60s' })
+                        res.status(200).json({ accessToken })
+                    }
+                }
+            )
+        }
+    }
+
+
+}
+
+//admin
+const LoginWithAdmin = async (req, res) => {
+    let data = req.body;
+
+    let user = await db.User.findOne({ where: { Phone: data.Phone } })
+
+    if (user) {
+        let check = await bcrypt.compareSync(data.PassWord, user.PassWord);
+        if (check) {
+            if (data.RoleID === "admin") {
+
+                let token = jwt.sign({ data: { Name: user.Name, Phone: user.Phone, PassWord: "Not Show", RoleID: "Not Show" } }, process.env.JWT_ADMIN_SECRET, { expiresIn: '1 days' });
+
+                return res.status(200).json({
+                    message: "success",
+                    token,
+                });
+            }
+            return res.status(200).json({
+                message: "Error phone number or password! Check your phone number and password, Please!"
+            });
+        }
+        else {
+            return res.status(200).json({
+                message: "Error phone number or password! Check your phone number and password, Please!"
+            });
+        }
+    }
+    return res.status(400).json({ message: "Error phone number or password! Check your phone number and password, Please!" });
+
+}
+
+const AuthTokenAdmin = (req, res, next) => {
+
+    const AuthHeader = req.headers['authorization'];
+
+    const token = AuthHeader.split(' ')[1];
+    if (!token) {
+        res.Status(200).json({ message: "Login field!" });
+    }
+    jwt.verify(token, process.env.JWT_ADMIN_SECRET, (err, data) => {
+
+        if (err) {
+            res.status(403)
+        }
+        else {
+            res.status(200).json({
+                data
+            })
+        }
+    })
+}
+
+module.exports = {
+    LoginWithUsers, LoginWithAdmin, AuthTokenUsers, LogoutWithUsers, RefreshWithUsers, AuthTokenAdmin
+}
